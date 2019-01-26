74410ceb0f3e1233faf40a3b41cbd9600e81aa3f
KSM06-13 <sohye_park@naver.com>
2019-01-26 17:45:01 +0900

    angular 에서 만든 date 파이프 이용해서 updated 출력

--- src/app/mydate.pipe.ts
+++ src/app/mydate.pipe.ts
@@ -5,10 +5,10 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class MydatePipe implements PipeTransform {
   // value: 파이프 앞의 변수를 가리킨다.
-  
+  // ars: mydate: 다음 변수를 가리킨다.
   // return: 새로운 출력
   transform(value: any, args?: any): any {
-    console.log(value);
+    console.log(value, args);
     return (value as string).substring(0, 16);
   }
 
