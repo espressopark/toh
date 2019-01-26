f99dd7a416168442a1ba48a2451989858a2fdf6a
KSM06-13 <sohye_park@naver.com>
2019-01-26 17:24:36 +0900

    todo - 날짜 파이프 적용해서 created 수정

---
+++ src/app/mydate.pipe.ts
@@ -0,0 +1,15 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'mydate'
+})
+export class MydatePipe implements PipeTransform {
+  // value: 파이프 앞의 변수를 가리킨다.
+  
+  // return: 새로운 출력
+  transform(value: any, args?: any): any {
+    console.log(value);
+    return (value as string).substring(0, 16);
+  }
+
+}
