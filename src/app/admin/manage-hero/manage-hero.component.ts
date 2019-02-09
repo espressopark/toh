3c75e7ace4633e8f8f2a81ab04621ea6e460eba7
KSM06-13 <sohye_park@naver.com>
2019-02-09 18:17:30 +0900

    hero 삭제

--- src/app/admin/manage-hero/manage-hero.component.ts
+++ src/app/admin/manage-hero/manage-hero.component.ts
@@ -3,6 +3,8 @@ import {HeroService} from '../../hero.service';
 import {Hero} from '../../hero';
 import {NgbModal} from '@ng-bootstrap/ng-bootstrap';
 import {ManageDialogComponent} from './manage-dialog/manage-dialog.component';
+import {AdminService} from '../admin.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-manage-hero',
@@ -12,7 +14,8 @@ import {ManageDialogComponent} from './manage-dialog/manage-dialog.component';
 export class ManageHeroComponent implements OnInit {
   heroes: Hero[]
 
-  constructor(private heroService: HeroService, private modalService: NgbModal) { }
+  constructor(private heroService: HeroService, private modalService: NgbModal,
+              private adminService: AdminService, private toaster: ToastrService) { }
 
   ngOnInit() {
     this.getHeroes();
@@ -26,5 +29,22 @@ export class ManageHeroComponent implements OnInit {
   confirmDelete(hero: Hero) {
     const dialogRef = this.modalService.open(ManageDialogComponent);
     dialogRef.componentInstance.name = hero.name;
+
+    dialogRef.result
+      .then(result => {
+        console.log(result);
+        if (result) {
+          // 삭제 로직
+          this.adminService.removeHero(hero.hero_id)
+            .subscribe(body => {
+              console.log(body);
+              if (body.result === 0) {
+                const index = this.heroes.findIndex(item => item.hero_id === hero.hero_id);
+                this.heroes.splice(index, 1);
+                this.toaster.info('success', '삭제되었습니다.');
+              }
+            });
+        }
+      });
   }
 }
