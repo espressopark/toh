1798beeecc92a985e815e53b3dd6d56b3d95f14f
KSM06-13 <sohye_park@naver.com>
2019-02-09 14:51:26 +0900

    리액티브 스타일 form 바인딩

--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -4,19 +4,23 @@ import { IndexComponent } from './index/index.component';
 import { RegisterHeroComponent } from './register-hero/register-hero.component';
 import { ManageHeroComponent } from './manage-hero/manage-hero.component';
 import {RouterModule, Routes} from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import {ReactiveFormsModule} from '@angular/forms';
 
 const routes: Routes = [
   {path: '', component: IndexComponent, children: [
-      {path: 'regiser', component: RegisterHeroComponent},
+      {path: '', component: DashboardComponent},
+      {path: 'register', component: RegisterHeroComponent},
       {path: 'manage', component: ManageHeroComponent}
     ]}
 ];
 
 @NgModule({
-  declarations: [IndexComponent, RegisterHeroComponent, ManageHeroComponent],
+  declarations: [IndexComponent, RegisterHeroComponent, ManageHeroComponent, DashboardComponent],
   imports: [
     CommonModule,
     RouterModule.forChild(routes),
+    ReactiveFormsModule,
   ]
 })
 export class AdminModule { }
